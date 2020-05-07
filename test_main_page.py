d22a848c31f29ee8800d01f8f85a54566dfb4a70
karmagen <karmagen9@gmail.com>
2020-05-07 21:12:52 +0300

    check login and register form

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -12,3 +13,10 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
     
+def test_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page1 = MainPage(browser, link)
+    page.open()                      # открываем страницу
+    page1.go_to_login_page()
+    page.should_be_login_page()          # выполняем метод страницы - переходим на страницу логина
